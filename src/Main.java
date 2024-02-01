414283d26b7af019f06fe8c8888ef7ef1471ce16
KolyanGrom <kolyangrom@yandex.ru>
2024-02-01 18:53:24 +0700

    second commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int ticket = 15555;
+        int x = 20;
+        int bonuse = 1;
+
+        System.out.println("Количество бонусеых милей с покупки в 15555 рублей:");
+        System.out.println(ticket / x * bonuse);
+
+    }
+}
