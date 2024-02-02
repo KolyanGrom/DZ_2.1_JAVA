afeac688cbcc93d917479a701a29b43653a1bb7c
KolyanGrom <kolyangrom@yandex.ru>
2024-02-02 16:23:37 +0700

    исправление ошибок

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
 
 
         int ticket = 15555;
-        int x = 20;
+        int stepplus = 20;
         int bonuse = 1;
 
         System.out.println("Количество бонусеых милей с покупки в 15555 рублей:");
-        System.out.println(ticket / x * bonuse);
+        System.out.println(ticket / stepplus * bonuse);
 
     }
 }
