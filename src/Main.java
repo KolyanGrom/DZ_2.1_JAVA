4594df41bf71afdee94aa4e34dad1e0438f0d580
KolyanGrom <kolyangrom@yandex.ru>
2024-02-04 20:55:40 +0700

    Изменение названий в значениях Пополнения и Бонусов

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
 
 
         int ticket = 15555;
-        int stepplus = 20;
-        int bonuse = 1;
+        int stepPlus = 20;
+        int bonus = 1;
 
         System.out.println("Количество бонусеых милей с покупки в 15555 рублей:");
-        System.out.println(ticket / stepplus * bonuse);
+        System.out.println(ticket / stepPlus * bonus);
 
     }
 }
